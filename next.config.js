9981812898f7031e5537b2d657750b86a8610e17
Randall <randall@inator.biz>
2023-06-29 23:02:00 -0700

    Fixing `next build`

--- next.config.js
+++ next.config.js
@@ -3,14 +3,14 @@ const withMarkdoc = require('@markdoc/next.js')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-    output: "export",,
+  output: 'standalone',
   pageExtensions: ['js', 'jsx', 'md'],
   experimental: {
     scrollRestoration: true,
   },
-    images: {
+  images: {
     unoptimized: true,
   },
-};
+}
 
 module.exports = withMarkdoc()(nextConfig)
