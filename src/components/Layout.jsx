25de2542a2587dad9ebc0fb9ecc4cedab4e6d569
Randall <randall@inator.biz>
2023-07-07 18:26:20 -0700

    add ALPHA flag to layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -55,6 +55,17 @@ function Header({ navigation }) {
           <Logomark className="h-9 w-9 lg:hidden" />
           <Logo className="hidden h-9 fill-slate-700 dark:fill-sky-100 lg:block" />
         </Link>
+        <div className="ml-5 font-display dark:text-slate-400">
+        <div className="ml-5">
+        <div className="ml-5">
+        <div className="ml-5">
+        <div className="ml-5">
+            ALPHA - PLEASE SUPPORT OUR CATALYST PROPOSALS
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
       </div>
       <div className="-my-5 mr-6 sm:mr-8 md:mr-0">
         {/* <Search /> */}
